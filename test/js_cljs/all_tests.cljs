7e56eb63af2165ab36d7ed8329c1e98757dd68c5
Maurício Szabo <mauricio.szabo@gmail.com>
2022-02-02 18:00:57 -0300

    Fixing some tests

--- test/js_cljs/all_tests.cljs
+++ test/js_cljs/all_tests.cljs
@@ -1,7 +1,8 @@
 (ns js-cljs.all-tests
   (:require [clojure.test :as test]
 
-            [js-cljs.simple-ops-test]))
+            [js-cljs.simple-ops-test]
+            [js-cljs.destructuring-test]))
 
 (defn ^:dev/after-load run-tests []
   (test/run-all-tests #"js-cljs.*-test"))
