ac2d53510e9daa115bf3edf3c4f65c500183ddd6
Maurício Szabo <mauricio.szabo@gmail.com>
2022-02-06 15:48:24 -0300

    Debug info

--- src/js_cljs/node.cljs
+++ src/js_cljs/node.cljs
@@ -14,8 +14,16 @@
 (def ^:private current-filename js/__filename)
 
 (defn main [ & args]
-  (p/let [in (readlines)]
-    (println (parse-str in {:style [:community]
-                            :parse {:interpose "\n\n"}
-                            :width 60
-                            :pair {:nl-separator? true}}))))
+  (p/let [in (readlines)
+          debug (atom nil)]
+    (try
+      (println (parse-str in {:zprint-opts {:style [:community]
+                                            :parse {:interpose "\n\n"}
+                                            :width 60
+                                            :pair {:nl-separator? true}}
+                              :format-opts {:debug debug}}))
+      (catch :default e
+        (println "Error parsing JS file")
+        (println "Last line to parse:" (count (.split (subs in 0 (:start @debug)) "\n")))
+        (println "\nException:\n" e)
+        (js/process.exit 1)))))
